310b45c08da93fe9732c5c82dc5c541ddf7e270f
jaimeCardenas176 <jaime.cardenas176gmail.com>
2018-03-21 14:30:57 +0100

    initial commit 2

---
+++ android_skypet/app/src/main/java/com/example/jaime/android_skypet/Registro2Activity.java
@@ -0,0 +1,13 @@
+package com.example.jaime.android_skypet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Registro2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro2);
+    }
+}
